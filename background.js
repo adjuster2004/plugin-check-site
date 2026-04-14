9a8c62da2c8445c65faf9f9a2bd246b5dee5ccc3
Sergey S <adjuster2004@gmail.com>
2026-04-14 11:13:45 +0300

    add icon.png

--- background.js
+++ background.js
@@ -70,7 +70,7 @@ async function runBackgroundCheck() {
             if (problemDomains.length > 0) {
                 chrome.notifications.create({
                     type: "basic",
-                    iconUrl: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚠️</text></svg>",
+                    iconUrl: "icon.png",
                     title: "Внимание! Проблемы с SSL",
                     message: `Сайты требуют внимания:\n${problemDomains.join(', ')}`,
                     priority: 2
